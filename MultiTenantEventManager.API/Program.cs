8f1cdc44cad9e2361024d7d8f16809d22696f56c
kursad <kursad.demirdogen@gmail.com>
2025-03-25 15:34:08 +0300

    swagger güncellendi

--- MultiTenantEventManager.API/Program.cs
+++ MultiTenantEventManager.API/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MultiTenantEventManager.API;
+using MultiTenantEventManager.Application.Abstractions.Services;
 using MultiTenantEventManager.Persistence.Contexts;
 using MultiTenantEventManager.Persistence.Services;
 using System.Text;
@@ -46,7 +48,41 @@ builder.Services.AddApplicationServices();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Web Api Swagger",
+        Description = "Sample ASP.NET Core Web API Swagger implementation",
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer 12345abcdef\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
 builder.Services.AddHttpContextAccessor();
 
 //veri tabaný baðlantýsý
